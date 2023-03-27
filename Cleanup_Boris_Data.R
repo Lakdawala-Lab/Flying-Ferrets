e26dbd56dd485e45a3096fe95a08e21d6da2a884
Sean Tavares <stavares@andrew.cmu.edu>
2023-03-27 16:26:12 -0400

    New Script

--- Cleanup_Boris_Data.R
+++ Cleanup_Boris_Data.R
@@ -1,16 +1,29 @@
 # Import libraries
 library(dplyr)
 
+# Accepting user input
+
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#print(args)
+
 # Open the .csv file (Betsur)
-data = read.csv("Betsur.Exposure1.Take2.csv")
+# must specify the # of rows to skip in the data file
 
-# Remove the title rows (first 20 rows)
-data = data[-c(1:20), ]
+data.file = args[1]#"../omkar/omkar.csv"
 
-# Set the first row of data to be the header/column names of the dataframe
-column.names = data[1,]
-names(data) = column.names
-data = data[-1, ]  # Remove the first row which contains the column names 
+print(data.file)
+
+rows_to_skip = args[2]
+
+print(rows_to_skip)
+
+data.file = "../omkar/omkar.csv"
+
+rows_to_skip = 21
+
+data = read.csv(data.file, header = T, skip = rows_to_skip)
 
 # For computation of duration, convert the datatype of `Time` from chr to numeric 
 data <- data %>% mutate(Time = as.numeric(Time)) 
@@ -25,7 +38,7 @@ data.problems <- data %>% filter(`Modifier 1` == "")
 # Clean the data  
 data.cleaned <- data %>%
   # Organize the data by columns that can uniquely identify a ferret's behavior 
-  arrange(Subject, Behavior, `Behavioral category`, `Modifier 1`) %>% 
+  arrange(Subject, Behavior, Behavioral.category, Modifier.1) %>% 
   # Add row numbers (to assist following computation)
   mutate(row_id=row_number()) %>% 
   # Group by every 2 rows (START, STOP)
@@ -43,7 +56,7 @@ data.cleaned <- data %>%
   # Remove Column that tracks Groups
   subset(select = -c(grp)) %>%
   # Select only relevant remaining columns
-  select(c("Start_Time", "End_Time", "Duration", "Subject", "Behavior", "Behavioral category", "Modifier 1", "Comment")) %>%
+  subset(select = c("Start_Time", "End_Time", "Duration", "Subject", "Behavior", "Behavioral.category", "Modifier.1", "Comment")) %>%
   # Order by Start_Time so that rows are ordered by the start of the event
   arrange(Start_Time)
 
